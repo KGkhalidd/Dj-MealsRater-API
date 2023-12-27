22bf967b118855d2f68da05a9f5d273e5aa4932b
khaled gamal <khaledgamal1345@gmail.com>
2023-12-27 05:33:33 +0200

    created the models and displayed it through admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Meal, Rating
 
-# Register your models here.
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'meal', 'user', 'stars')
+    list_filter = ('meal', 'user', 'stars')
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    list_filter = ('title', 'description')
+    search_fields = ('title', 'description')
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Meal(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.stars} stars for {self.meal}"
+    
+    class Meta:
+        unique_together = (('user', 'meal'),)
+        index_together = (('user', 'meal'),)
