653f43efb67f62274b7ca955ce614d1d33592ad1
khaled gamal <khaledgamal1345@gmail.com>
2023-12-27 06:24:11 +0200

    add field to the models to order them with

--- api/admin.py
+++ api/admin.py
@@ -3,11 +3,11 @@ from .models import Meal, Rating
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'meal', 'user', 'stars')
+    list_display = ('id', 'meal', 'user', 'stars', 'created')
     list_filter = ('meal', 'user', 'stars')
 
 @admin.register(Meal)
 class MealAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description')
+    list_display = ('id', 'title', 'description', 'created')
     list_filter = ('title', 'description')
     search_fields = ('title', 'description')
--- api/models.py
+++ api/models.py
@@ -5,6 +5,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Meal(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
 
     def __str__(self):
         return self.title
@@ -13,10 +17,12 @@ class Rating(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.stars} stars for {self.meal}"
     
     class Meta:
+        ordering = ['-created']
         unique_together = (('user', 'meal'),)
         index_together = (('user', 'meal'),)
